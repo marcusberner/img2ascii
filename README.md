0b663b66bbb835214b7923741a30f3cdf6392cf4
Marcus Berner <marcus@burner.se>
2014-04-19 09:15:05 +0200

    Added font size control and example

--- README.md
+++ README.md
@@ -3,11 +3,13 @@ img2ascii
 
 Converts all images with the `data-img2ascii` attribute to ASCII art.
 
+The font size of the ASCII art can be controlled by providing an integer value in the `data-ascii-font-size` attribute. If no font size is provided the default size of 4px will be used.
+
 ```html
-<img data-img2ascii src="image.jpg" />
+<img data-img2ascii data-ascii-font-size="2" src="image.jpg" />
 ```
 
-The image gets replaced with a `<div>` containing ASCII art. Any CSS class gets transferred from the image to the div.
+The image gets replaced with a `<div>` containing ASCII art. Any CSS class gets transferred from the img tag to the div tag.
 
 ### Prerequisites and limitations
 
--- img2ascii.js
+++ img2ascii.js
@@ -1,19 +1,19 @@
 (function () {
 
-	function convertToAscii(img) {
+	function convertToAscii(img, fontSize) {
 
 		var
 			ctx,
 			matchChar,
-			charWidth = 5,
-			charHeight = 6,
+			charHeight = Math.round(fontSize * 6 / 8),
+			charWidth = fontSize * 5 / 8,
 			parent = img.parentNode,
 			imageClass,
-			canvasNode = document.createElement("canvas"),
-			asciiNode = document.createElement("div");
+			canvasNode = document.createElement('canvas'),
+			asciiNode = document.createElement('div');
 
 		matchChar = function (data, x, y) {
-			var chars, r, g, b, alpha, darkness, charData = [];
+			var chars, r, g, b, alpha, darkness;
 			r = data[((img.clientWidth * y) + x) * 4];
 			g = data[((img.clientWidth * y) + x) * 4 + 1];
 			b = data[((img.clientWidth * y) + x) * 4 + 2];
@@ -23,26 +23,28 @@
 			return chars[Math.min(chars.length - 1, Math.floor(darkness * chars.length / 255))];
 		};
 
-		asciiNode.setAttribute('style', 'display:block;overflow:hidden;font-family:monospace;font-size: 8px;line-height: 6px;letter-spacing:0px;');
+		asciiNode.setAttribute('style', 'display:block;overflow:hidden;font-family:monospace;font-size: ' + fontSize + 'px;line-height: ' + charHeight + 'px;letter-spacing:0px;');
 		imageClass = img.getAttribute("class");
 		if (imageClass) asciiNode.setAttribute('class', imageClass);
 		canvasNode.setAttribute('style', 'display:none;');
 		parent.insertBefore(canvasNode, img);
-		ctx = canvasNode.getContext("2d");
+		ctx = canvasNode.getContext('2d');
 
 		img.onload = function() {
+			var x, y, data, lines, line;
 			canvasNode.height = img.clientHeight;
 			canvasNode.width = img.clientWidth;
 			ctx.drawImage(img, 0, 0, img.clientWidth, img.clientHeight);
-			var data = ctx.getImageData(0, 0, img.clientWidth, img.clientHeight).data;
-			var result = '';
-			for(var y = 0; y <= (img.clientHeight - charHeight); y += charHeight) {
-				for(var x = 0; x <= (img.clientWidth - charWidth); x += charWidth) {
-					result += matchChar(data, x, y);
+			data = ctx.getImageData(0, 0, img.clientWidth, img.clientHeight).data;
+			lines = [];
+			for(y = 0; y <= (img.clientHeight - charHeight); y += charHeight) {
+				line = [];
+				for(x = 0; x <= (img.clientWidth - charWidth); x += charWidth) {
+					line.push(matchChar(data, Math.round(x), y));
 				}
-				result += '<br/>';
+				lines.push(line.join(''));
 			}
-			asciiNode.innerHTML = result;
+			asciiNode.innerHTML = lines.join('<br/>');
 			parent.removeChild(canvasNode);
 			parent.insertBefore(asciiNode, img);
 			parent.removeChild(img);
@@ -52,10 +54,14 @@
 
 	if (!window.WebGLRenderingContext) return;
 	document.addEventListener('DOMContentLoaded', function () {
-		var images = document.getElementsByTagName('img');
-		for (var i = 0; i < images.length; i++) {
-			var attr = images[i].getAttribute("data-img2ascii");
-			if (attr || attr === '') convertToAscii(images[i], attr === 'simple');
+		var i, shouldConvert, fontSize, images = document.getElementsByTagName('img');
+		for (i = 0; i < images.length; i++) {
+			shouldConvert = images[i].getAttribute('data-img2ascii');
+			if (!shouldConvert && shouldConvert !== '') return;
+			fontSize = images[i].getAttribute('data-ascii-font-size');
+			if (fontSize) fontSize = parseInt(fontSize, 10);
+			else fontSize = 4;
+			convertToAscii(images[i], fontSize);
 		}
 	}, false);
 
